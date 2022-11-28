a4684012666b0222328220f8b2eba7f0bf806ede
Shar McLeod <shr.mcl@gmail.com>
2022-11-28 13:15:35 -0600

    set up for code redo

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
+// RE-DOING THE CODE FROM MEMORY
+
 console.log('testing linkage')
-// select the form
-const form = document.querySelector('#todo-form')
 
-// listen for submit
-form.addEventListener('submit', (evt) => {
-    // prevent page refresh on submit
-    evt.preventDefault();
+// select the input text
+// listen for submit (and prevent defualt)
+// create new list item element and assign its value to the input text
+// add the new list item to the list
+// clear the input value
 
-    // on submit, select the input text
-    const input = document.querySelector('#todo-input')
 
-    // test the input
-    alert(input.value)
-    const list = document.querySelector('#todo-list')
 
-    // create a new list itme with that text
-    const newLi = document.createElement('li')
-    newLi.innerText = input.value
 
-    // add that list item to the list
-    list.append(newLi)
 
-    // clear text input
-    input.value = ''
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CLASS DEMO CODE:
+
+// console.log('testing linkage')
+// // select the form
+// const form = document.querySelector('#todo-form')
+
+// // listen for submit
+// form.addEventListener('submit', (evt) => {
+//     // prevent page refresh on submit
+//     evt.preventDefault();
+
+//     // on submit, select the input text
+//     const input = document.querySelector('#todo-input')
+
+//     // test the input
+//     alert(input.value)
+//     const list = document.querySelector('#todo-list')
+
+//     // create a new list itme with that text
+//     const newLi = document.createElement('li')
+//     newLi.innerText = input.value
+
+//     // add that list item to the list
+//     list.append(newLi)
+
+//     // clear text input
+//     input.value = ''
+// })
